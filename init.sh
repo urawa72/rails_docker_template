7a896d380dde9134aaf634b60a85e41f002423c8
urawa72 <{urawa72@gmail.com}>
2019-08-11 15:58:13 +0900

    init.shをfrontendブランチ向けに修正

--- init.sh
+++ init.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-DIR="app"
 cd /myapp
 
-# app/がないときだけ初回のrailsアプリ生成処理
-if [ ! -d $DIR ]; then
-  rails new . --force --database=mysql --skip-bundle
-  # database.yml設定
-  cat config/database.yml | sed 's/password:$/password: root/' | sed 's/host: localhost/host: db/' > __tmpfile__
-  cat __tmpfile__ > config/database.yml
-  rm __tmpfile__
-  # rails起動
-  bundle install --path vendor/bundle
+# yarn未インストールの場合の初期処理
+which yarn > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  npm install -g yarn
+  yarn install
   bundle exec rails db:create
   bundle exec rails db:migrate
-  exec bin/bash
-  # bundle exec rails s -b 0.0.0.0
 else
   cd /
   exec bin/bash
 fi
-

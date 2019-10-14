6e2ae416143f45c29133ec331a560c1c144d1e9b
urawa72 <urawa72@gmail.com>
2019-10-14 16:30:58 +0900

    rails6に対応

--- init.sh
+++ init.sh
@@ -6,14 +6,23 @@ cd /myapp
 # app/がないときだけ初回のrailsアプリ生成処理
 if [ ! -d $DIR ]; then
   rails new . --force --database=mysql --skip-bundle
+
+  # bundle install
+  bundle install --path vendor/bundle
+
+  # yarn & webpacker
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+  apt-get update && apt-get install yarn
+  bundle exec rails webpacker:install
+
   # database.yml設定
   cat config/database.yml | sed 's/password:$/password: root/' | sed 's/host: localhost/host: db/' > __tmpfile__
   cat __tmpfile__ > config/database.yml
   rm __tmpfile__
-  # rails起動
-  bundle install --path vendor/bundle
   bundle exec rails db:create
   bundle exec rails db:migrate
+
   bundle exec rails s -b 0.0.0.0
 else
   cd /
